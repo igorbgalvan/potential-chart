f485a1e0a3a8b26bad2aefd8696e0490a33a1191
Igor Braga <igorbgalvan@hotmail.com>
2021-08-30 18:38:15 -0400

    grid size fixed

--- src/components/PotentialChart.vue
+++ src/components/PotentialChart.vue
@@ -7,7 +7,7 @@
         bg-white
         shadow-xl
         rounded-xl
-        border border-grey-300
+        border border-gray-300
       "
     >
       <div class="divide-y divide-gray-200 divide-solid overflow-hidden">
@@ -16,16 +16,21 @@
         </div>
         <div>
           <div class="overflow-auto md:overflow-visible md:m-6">
-            <div class="flex flex-row-2">
-              <div>
-                <div class="m-2 flex flex-col-2 justify-center items-center">
-                  <div class="">
-                    <p class="w-6 text-gray-500 transform -rotate-90">
-                      People/Culture
-                    </p>
-                  </div>
-                  <div class="grid grid-cols-4 gap-1">
-                    <div class="h-24 w-24 bg-yellow-300 rounded-tl-lg"></div>
+            <div class="flex flex-row">
+              <div class="m-2 flex flex-row justify-center items-center">
+                <div class="flex-shrink text-gray-500 transform -rotate-90">
+                  People/Culture
+                </div>
+                <div class="overflow-auto">
+                  <div
+                    class="grid grid-cols-4 relative"
+                    style="width: 400px; height: 400px"
+                    ref="grid"
+                  >
+                    <div
+                      class="bg-yellow-300 rounded-tl-lg"
+                      style="width: 96px; height: 96px"
+                    ></div>
                     <div class="h-24 w-24 bg-blue-300"></div>
                     <div class="h-24 w-24 bg-green-300"></div>
                     <div class="h-24 w-24 bg-green-300 rounded-tr-lg"></div>
@@ -44,11 +49,6 @@
                     <div class="h-24 w-24 bg-red-300"></div>
                     <div class="h-24 w-24 bg-yellow-300"></div>
                     <div class="h-24 w-24 bg-yellow-300 rounded-br-lg"></div>
-                  </div>
-                </div>
-                <div>
-                  <div class="flex -mt-2 flex-col items-center justify-center">
-                    <p class="w-6 text-gray-500">Skill/Experience</p>
                   </div>
                 </div>
               </div>
@@ -87,7 +87,58 @@
 export default {
   name: "Potential Chart",
   data() {
-    return {};
+    return {
+      data: [
+        {
+          x: 200,
+          y: 200,
+          entries: [
+            {
+              name: "Daniel",
+              hard: 2.1,
+              soft: 2,
+              picture: "https://i.pravatar.cc/150?u=igorbraga",
+            },
+            {
+              name: "igor",
+              soft: 2.1,
+              hard: 2,
+              img: "https://i.pravatar.cc/300",
+            },
+          ],
+        },
+        {
+          x: 220,
+          y: 220,
+          entries: [],
+        },
+        {
+          x: 100,
+          y: 100,
+          entries: [],
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.data.forEach((element) => {
+      const el = document.createElement(`div`);
+      if (element.entries.length > 1) {
+        el.setAttribute("class", "absolute bg-red-900 z-10");
+        el.setAttribute(
+          "style",
+          ` width: 32px; height: 32px; top: ${element.y}px; right: ${element.x}px;`
+        );
+        this.$refs.grid.appendChild(el);
+      } else {
+        el.setAttribute("class", "absolute bg-red-900 z-10");
+        el.setAttribute(
+          "style",
+          ` width: 32px; height: 32px; top: ${element.y}px; right: ${element.x}px;`
+        );
+        this.$refs.grid.appendChild(el);
+      }
+    });
   },
 };
 </script>
